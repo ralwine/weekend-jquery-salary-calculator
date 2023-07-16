7b6dd998cf1e7d85071886f6d9e0b0c9fb0c857a
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-16 17:37:22 -0500

    developingfunctions

--- wjsc.js
+++ wjsc.js
@@ -2,5 +2,31 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('in onReady');
+
+   $('.submit').on('click', addFirstName);
+   $('.submit').on('click', addLastName);
+   $('.submit').on('click', addID);
+   $('.submit').on('click', addTitle);
+   $('.submit').on('click', addAnnualSalary);
+
 }
 
+function addFirstName() {
+
+}
+
+function addLastName() {
+
+}
+
+function addID() {
+
+}
+
+function addTitle() {
+
+}
+
+function addAnnualSalary() {
+
+}
