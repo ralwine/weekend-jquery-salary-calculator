bf954726abcb04df43a3ad36c09e9e78866a107e
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-31 21:09:57 -0500

    submissions are appending to DOM

--- wjsc.js
+++ wjsc.js
@@ -22,5 +22,20 @@ function addEmployee(event) {
         annualSalary: $('#annualSalary').val()// this is a string here
     }
     console.log("Employee is: ", employee)
+    render(employee)
 }
 
+function render(employee) {
+    console.log("Render responding")
+    // Respodning... #salaryTable needs to be targeted to append
+    $('#salaryTable').append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNumber}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+        </tr>
+
+    `)
+}
